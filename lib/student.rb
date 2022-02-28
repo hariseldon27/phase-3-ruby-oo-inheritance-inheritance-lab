d18cb9b7d66aff0dae3afa7f942b66318de272cf
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-28 08:02:01 -0800

    commit tree 2048893c33a06fef8231ac41227d1779bb10ccd7

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,9 @@
-class Student 
+require_relative './user.rb'
 
+class Student < User
+
+    def initialize(knowledge_string = "stringy")
+        @knowledge = []
+        self.knowledge << knowledge_string
+    end
 end
